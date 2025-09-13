4539bed48eb873270d9213de9d34c5824fea5c16
Omar Mostafa <OmarMostafa6103@email.com>
2025-09-13 20:33:14 +0300

    fix(router): use HashRouter for GitHub Pages compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -22,7 +22,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -30,9 +30,9 @@ import { PersistGate } from 'redux-persist/integration/react';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </PersistGate>
   </Provider>,
 );
